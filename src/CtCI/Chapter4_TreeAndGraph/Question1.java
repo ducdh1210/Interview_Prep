f8df90e0429b35f42596a5927e714d5ab9390057
Duc Do <duc.do@marquette.edu>
2014-10-15 21:34:26 -0500

    add new answers

---
+++ src/CtCI/Chapter4_TreeAndGraph/Question1.java
@@ -0,0 +1,9 @@
+package CtCI.Chapter4_TreeAndGraph;
+
+/**
+ * Check if a tree is balanced.
+ * A balanced tree is defined to be a tree such that the heights of any two subtree never differs more than 1
+ */
+public class Question1 {
+
+}
