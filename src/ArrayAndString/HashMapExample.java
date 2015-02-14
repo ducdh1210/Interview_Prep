22dec887187618cee5eaf4bf0b6d4a2803847a97
Duc Do <duc.do@marquette.edu>
2015-02-14 17:09:40 -0600

    add HashMapExample

--- src/ArrayAndString/HashMapExample.java
+++ src/ArrayAndString/HashMapExample.java
@@ -10,13 +10,16 @@ public class HashMapExample {
 	        map.put(21,"Twenty one");
 	        map.put(31, "Thirty One");
 	        Iterator<Integer> keySetIterator  = map.keySet().iterator();
-	        while(keySetIterator.hasNext()){
+
+         while(keySetIterator.hasNext()){
 	            Integer thisKey = keySetIterator.next();
 	            System.out.println("Key: " + thisKey + " value: " + map.get(thisKey));
 	        }
 	 }
 
     public static void main(String[] args) {
+
+
         String s1 = "I love you";
         // convert String to char array
         char[] charArray = s1.toCharArray();
